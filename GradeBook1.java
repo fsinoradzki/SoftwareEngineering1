6c0248d8726f71627948d6de81957878c83f5c52
BenNBen <barriageb@gmail.com>
2016-10-22 14:18:32 -0400

    Newest Version of GradeBook1.java with working vector implementation and file I/O

--- GradeBook1.java
+++ GradeBook1.java
@@ -63,46 +63,101 @@ public class GradeBook1 {
     
 
 	public static class Class {
-		private Vector<int>[][] gradebook; //Matrix that stores the student ID as the first
-					   //As the first element in every row. Every other
-					   //cell is a grade
+		private Vector<Vector<Integer>> gradebook; //Vector matrix that stores the student ID as the first
+					   		   //As the first element in every row. Every other
+					   		   //cell is a grade
 		private int numHWs;        //# of homework assignments
 		private int numQuizzes;    //# of quizzes
 		private int numLabs;       //# of lab assignments
 		private Rubric R;          //The rubric for the class
 		
 		//Constructor
-		public Class(Vector<int>[][] SandG, int HWs, int Quizzes, int Labs){
-			gradebook = SandG;
-			numHWs = HWs;
-			numQuizzes = Quizzes;
-			numLabs = Labs;
-			R = new Rubric();}
-
-	    	
-		
-		//Prints out all grades for a specific student
-		public void getGradesForStudent(int ID){
-			for(int i=1;i<gradebook[ID].size();i++)
-					System.out.println("Grade: " + gradebook[ID][i]);
+	    public Class(Vector<Vector<Integer>> SandG, int HWs, int Quizzes, int Labs){
+		gradebook = new Vector<Vector<Integer>>(SandG);
+		numHWs = HWs;
+		numQuizzes = Quizzes;
+		numLabs = Labs;
+		R = new Rubric();}
+	    
+	    
+	    public Class (File file, int HWs, int Quizzes, int Labs){
+		try{	  
+		    Vector<Vector<Integer>>matrix = new Vector<Vector<Integer>>();
+		    BufferedReader br = new BufferedReader(new FileReader(file));
+		    String line;
+		    while((line = br.readLine())!=null){
+			StringTokenizer st = new StringTokenizer(line);
+			int num = 0;
+			Vector<Integer>test = new Vector<Integer>();
+			test.clear();
+			while(st.hasMoreTokens()){
+			    int value1 = Integer.parseInt(st.nextToken());
+			    test.addElement(value1);
+			    if(!st.hasMoreTokens())
+				matrix.addElement(test);
+			}
+		    }
+		    gradebook = matrix;	      
 		}
-
-		//Prints out all grades for a specific assignment
-		public void getGradesForClass(int assignment){
+		catch(IOException e){
+		}
+	    }
+	    
+	    public void createFile(File file){
+		try{
+		    if(!file.exists()){
+			file.createNewFile();
+		    }
+		}catch(IOException e){
+		}
+		Vector<Vector<Integer>>matrix = new Vector<Vector<Integer>>();
+		matrix=this.gradebook;
+		try{
+		    FileWriter fw = new FileWriter(file);
+		    BufferedWriter bw = new BufferedWriter(fw);
+		    for (int i =0;i<matrix.size();i++){
+			for(int k =0;k<(matrix.get(i).size());k++){
+			    bw.write(String.valueOf((matrix.get(i)).get(k)));
+			    bw.write(" ");
+			}
+			bw.write("\n");
+		    }
+		    bw.close();
+		    
+		}
+		catch(IOException e){
+		}
+	    }
+	    
+	    
+	    //Prints the whole gradebook
+	    public void printGradebook(){
+		System.out.println(gradebook);}
+	    
+	    //Prints out all grades for a specific student
+	    public void getGradesForStudent(int ID){
+		for(int i=1;i<(gradebook.get(ID)).size();i++){
+		    //				System.out.println(gradebook.get(ID));
+		    System.out.println("Grade: " + ((gradebook.get(ID)).get(i)));
+		}
+	    }
+	    
+	    //Prints out all grades for a specific assignment
+	    public void getGradesForClass(int assignment){
 			if(assignment != 0){
-				for(int i=0;i<gradebook.size();i++)
-						System.out.println("Grade: " + gradebook[i][assignment]);
+			    for(int i=0;i<gradebook.size();i++)
+				System.out.println("Grade: " + (gradebook.get(i)).get(assignment));
 			} else
 				System.out.println("That's for student_id");
 		}
 		
 		//Prints out the highest grade for a specific assignment
 		public void printHighestGradeForAssignment(int assignment){
-			int max=gradebook[1][assignment];
+			int max=(gradebook.get(1)).get(assignment);
 			if(assignment != 0){
 				for(int i=1;i<gradebook.size();i++)
-					if(gradebook[i][assignment] > max)
-						max = gradebook[i][assignment];
+					if((gradebook.get(i)).get(assignment) > max)
+						max = (gradebook.get(i)).get(assignment);
 			} else
 				System.out.println("That's for student_id");
 				
@@ -111,11 +166,11 @@ public class GradeBook1 {
 		
 		//Prints out the lowest grade for a specific assignment
 		public void printLowestGradeForAssignment(int assignment){
-			int low=gradebook[1][assignment];
+			int low=(gradebook.get(1)).get(assignment);
 			if(assignment != 0){
 				for(int i=1;i<gradebook.size();i++)
-					if(gradebook[i][assignment] < low)
-						low = gradebook[i][assignment];
+					if((gradebook.get(i)).get(assignment) < low)
+						low = (gradebook.get(i)).get(assignment);
 			} else
 				System.out.println("That's for student_id");
 				
@@ -124,20 +179,20 @@ public class GradeBook1 {
 	
 		//Prints out the Student's highest grade
 		public void printHighestGradeForStudent(int ID){
-			int high = gradebook[ID][1];
-			for(int i=1;i<gradebook[ID].size();i++)
-				if(gradebook[ID][i] > high)
-					high = gradebook[ID][i];
+			int high = (gradebook.get(ID)).get(1);
+			for(int i=1;i<(gradebook.get(ID)).size();i++)
+				if((gradebook.get(ID)).get(i) > high)
+					high = (gradebook.get(ID)).get(i);
 					
 			System.out.println("The highest grade for Student " + ID + " is " + high);
 		}		
 		
 		//Prints out the Student's lowest grade
 		public void printLowestGradeForStudent(int ID){
-			int low = gradebook[ID][1];
-			for(int i=1;i<gradebook[ID].size();i++)
-				if(gradebook[ID][i] < low)
-					low = gradebook[ID][i];
+			int low = (gradebook.get(ID)).get(1);
+			for(int i=1;i<(gradebook.get(ID)).size();i++)
+				if((gradebook.get(ID)).get(i) < low)
+					low = (gradebook.get(ID)).get(i);
 					
 			System.out.println("The lowest grade for Student #" + ID + " is " + low);
 		}
@@ -147,20 +202,20 @@ public class GradeBook1 {
 			int total = 0;
 			if(assignment != 0)
 				for(int i=0;i<gradebook.size();i++)
-					total += gradebook[i][assignment];
+					total += (gradebook.get(i)).get(assignment);
 			 else
 				System.out.println("That's for student_id");
 
-			return (double) total/gradebook.length;
+			return (double) total/gradebook.size();
 		}
 		
 		//Returns the value of the average for a given student
 		public double getAverageForStudent(int ID){
 			int total=0;
-			for(int i=1;i<gradebook[ID].size();i++)
-				total += gradebook[ID][i];
+			for(int i=1;i<(gradebook.get(ID)).size();i++)
+				total += (gradebook.get(ID)).get(i);
 			
-			return (double) total/(gradebook[ID].size()-1);	
+			return (double) total/((gradebook.get(ID)).size()-1);	
 		}
 		
 		//Gets Standard Deviation for a given assignment
@@ -168,14 +223,14 @@ public class GradeBook1 {
 			double total = 0;
 			double av=0;
 			
-			for(int i=0;i<gradebook.length;i++)
-				total += gradebook[i][assignment];
+			for(int i=0;i<gradebook.size();i++)
+				total += (gradebook.get(i)).get(assignment);
 				
 			av = total/gradebook.size();
 			total = 0;
 			
-			for(int i=0;i<gradebook.length;i++)
-				total += (gradebook[i][assignment]-av) * (gradebook[i][assignment]-av);
+			for(int i=0;i<gradebook.size();i++)
+				total += ((gradebook.get(i)).get(assignment)-av) * ((gradebook.get(i)).get(assignment)-av);
 				
 			av = total/gradebook.size();
 			av = Math.sqrt(av);
@@ -188,16 +243,16 @@ public class GradeBook1 {
 			double total = 0;
 			double av=0;
 			
-			for(int i=1;i<gradebook[ID].size();i++)
-				total += gradebook[ID][i];
+			for(int i=1;i<(gradebook.get(ID)).size();i++)
+				total += (gradebook.get(ID)).get(i);
 				
-			av = total/(gradebook[ID].size()-1);
+			av = total/((gradebook.get(ID)).size()-1);
 			total = 0;
 			
-			for(int i=1;i<gradebook[ID].size();i++)
-				total += (gradebook[ID][i]-av) * (gradebook[ID][i]-av);
+			for(int i=1;i<(gradebook.get(ID)).size();i++)
+				total += ((gradebook.get(ID)).get(i)-av) * ((gradebook.get(ID)).get(i)-av);
 				
-			av = total/(gradebook[ID].size()-1);
+			av = total/((gradebook.get(ID)).size()-1);
 			av = Math.sqrt(av);
 
 			return av;
@@ -208,8 +263,8 @@ public class GradeBook1 {
 			int total=0;
 			int av;
 			
-			for(int i=1;i<gradebook[ID].size();i++)
-				total += gradebook[ID][i];
+			for(int i=1;i<(gradebook.get(ID)).size();i++)
+				total += (gradebook.get(ID)).get(i);
 			av = total/((numHWs*R.getHWValue())+(numQuizzes*R.getQuizValue())+(numLabs*R.getLabValue()));
 			
 			return av;
@@ -217,50 +272,57 @@ public class GradeBook1 {
 	}
 
 	public static void main(String[] args) {
-	    Scanner user_input = new Scanner(System.in);
 	    
+	    File file = new File("grades.txt");
+	    File output = new File("output.txt");
 	    Student f = new Student("Frank",123);
 	    Student b = new Student("Ben",456);
 	    Student p = new Student("Phoebe",789);
 	    Student x = new Student("Dummy",000);
-	    
-	    System.out.println("Please enter the new student's name:");
-	    String name = user_input.next();
-	    System.out.println("Please enter the student ID #:");
-	    String identification = user_input.next();
-	    int iD = Integer.parseInt(identification);
-	    
-	    Student n = new Student(name, iD);
-	    System.out.println(n.getName());
-	    System.out.println(n.getID());
 		
-	    System.out.println(f.getName());
+	    Vector<Vector<Integer>> SandG = new Vector<Vector<Integer>>();
+	    Vector<Integer> test1 = new Vector<Integer>();
+	    Vector<Integer> test2 = new Vector<Integer>();
+	    Vector<Integer> test3 = new Vector<Integer>();
+	    Vector<Integer> test4 = new Vector<Integer>();
 	    
-	    try{
-		Vector<int>[][]SandG = new Vector<int>[4][4];
-		BufferedReader br = new BufferedReader(new FileReader(args[0]));
-		String line;
-		int i =0;
-		while((line = br.readLine())!=null){
-		    StringTokenizer st = new StringTokenizer(line);
-		    int num = 0;
-		    while(st.hasMoreTokens()){
-			int value1 = Integer.parseInt(st.nextToken());
-			SandG[i][num]=value1;
-			num++;
-		    }
-		    i++;
-		}
-		    
-		int HWs = 3;
-		int Quizzes = 0;
-		int Labs = 0;
-		
-		//Prints out Ben's grades
-		Class c1 = new Class(SandG, HWs, Quizzes, Labs);
-		System.out.println("Ben's grades:");
-		c1.getGradesForStudent(1);
-		
+		//Creates the initial 2D Vector
+	    test1.addElement(123);
+	    test1.addElement(10);
+	    test1.addElement(20);
+	    test1.addElement(30);
+	    SandG.addElement(test1);
+	    test2.addElement(456);
+	    test2.addElement(40);
+	    test2.addElement(50);
+	    test2.addElement(60);
+	    SandG.addElement(test2);
+	    test3.addElement(789);
+	    test3.addElement(70);
+	    test3.addElement(80);
+	    test3.addElement(90);
+	    SandG.addElement(test3);
+	    test4.addElement(000);
+	    test4.addElement(0);
+	    test4.addElement(0);
+	    test4.addElement(0);
+	    SandG.addElement(test4);
+	    
+	    int HWs = 3;
+	    int Quizzes = 0;
+	    int Labs = 0;
+	    
+	    //Prints out Ben's grades
+	    Class c2 = new Class(file, HWs, Quizzes, Labs);
+	    c2.createFile(output);
+	    c2.printGradebook();
+	    
+	    Class c1 = new Class(SandG, HWs, Quizzes, Labs);
+	    System.out.println("After creating c1: ");
+	    c1.printGradebook();
+	    System.out.println("Ben's grades:");
+	    c1.getGradesForStudent(1);
+	    
 		//Prints out all grades for assignment 1
 		System.out.println("Grades for assignment 1:");
 		c1.getGradesForClass(1);
@@ -282,8 +344,5 @@ public class GradeBook1 {
 		System.out.println("Stan Dev for assignment 1 = " + SD1);
 		double SD2 = c1.getStanDevForStudent(1);
 		System.out.println("Stan Dev for student 1 = " + SD2);
-		}
-		catch(Exception e){}
 	}
 }
-
